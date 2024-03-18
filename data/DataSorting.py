9c3ad1b2f43d4cf8e9bffa7a60a99c114ad28cb2
Dawid <dawid.hrynkiewicz@internet-plus.pl>
2024-03-18 23:12:59 +0100

    Method to calculate average volume

--- data/DataSorting.py
+++ data/DataSorting.py
@@ -6,34 +6,54 @@ class DataSorting(GetStockData):
     """
     A class to sort the data from file
     """
-    def __init__(self, main_dir: str, sub_dir: str,  file_name: str, sub_dir_folder: str = ''):
-        self.main_dir = main_dir
-        self.sub_dir = sub_dir
-        self.sub_dir_folder = sub_dir_folder
-        self.file_name = file_name
+    def __init__(self, main_dir: str, sub_dir: str,  file_name: str,
+                 stock_data_object: Callable = stock1.get_data_from_single_stock_file(),
+                 sub_dir_folder: str = '') -> None:
+
         super().__init__(
-            main_dir, sub_dir, sub_dir_folder, file_name
+            main_dir, sub_dir, sub_dir_folder, file_name,
         )
 
-    @staticmethod
-    def single_stock_data_segregation(stock_data: Callable) -> None:
+        self.stock_data_object = stock_data_object
+
+    def stock_data_segregation(self) -> dict:
         """
         Static method to get single columns from stock file
 
-        :param stock_data: Object which finds wanted file
-        :return: None
+        :return: dict with data from file
         """
 
-        data_list: Callable = stock_data
-        del data_list[0]
+        data_list: list[list[str]] = self.stock_data_object
         stock_ticket: str = (data_list[0][0])[:-3]
         date: str = data_list[0][1]
-        volume: int = data_list[0][6]
+        volume: str = data_list[0][6]
         open, high, low, close = data_list[0][2], data_list[0][3], data_list[0][4], data_list[0][5]
-        day, month, year = date[-2:], date[5:7], date[:4]
+        day, month, year = date[-2:], date[4:6], date[:4]
+        data_dict = {'stock_ticket': stock_ticket, 'date': date, 'volume': volume, 'open': open, 'high': high,
+                     'low': low, 'close': close, 'formatted_date': day + '.' + month + '.' + year}
+        return data_dict
 
-        print(stock_ticket, volume)
+    def calculate_average_volume_from_given_period(self, period: int, start_from_last: bool) -> float:
+        """
+        Method to calculate average stock volume from given period
+        :param period: range in day
+        :param start_from_last: start with last day quote or from historical start quote
+        :return: int: average volume
+        """
+        data_list: list[list[str]] = self.stock_data_object
+        data_list_len: int = len(data_list) - 1
+        vol_sum_from_last: int = 0
+        counter: int = 0
+        if start_from_last:
+            for i in range(data_list_len, data_list_len - period, -1):
+                vol_sum_from_last += int(data_list[i][6])
+                counter += 1
+        else:
+            for i in range(0, period):
+                vol_sum_from_last += int(data_list[i][6])
+                counter += 1
+        return vol_sum_from_last / counter
 
 
-data1 = DataSorting('data/daily/us', 'nasdaq_stocks', 'cdzi.us.txt', '1')
-print(data1.single_stock_data_segregation(stock1.get_data_from_single_stock_file()))
+data1 = DataSorting('data/daily/us', 'nasdaq_stocks', 'aaci.us.txt', sub_dir_folder='1')
+print(data1.calculate_average_volume_from_given_period(10, True))
